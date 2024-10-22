d9ae3d57cac42fbf5f7f7f176128996618ca6ac7
andresisfelipe20 <andresisfelipe20@gmail.com>
2024-10-22 22:32:34 +0000

    Add funcionality to nav items so data change acord to nav item clicked

--- time-tracking-dashboard/main.js
+++ time-tracking-dashboard/main.js
@@ -1,5 +1,5 @@
 const cardsContainer = document.querySelector('.activity-cards')
-
+const menuItems = document.querySelectorAll('.profile-card__stats-list-item')
 
 const data = [
     {
@@ -106,7 +106,27 @@ const data = [
     }
 ]
 
-function createCard({title, timeframes}){
+menuItems.forEach(item => {
+    item.addEventListener('click', () => {
+        menuItems.forEach(item => {
+            item.classList.remove('active')
+        })
+        item.classList.add('active')
+
+        const itemText = item.textContent.toLowerCase()
+        cardsContainer.innerHTML = ""
+
+        data.forEach(card => {
+            cardsContainer.appendChild(createCard({
+                title: card.title,
+                timeframes: card.timeframes,
+                time: itemText
+            }))
+        })
+    })
+})
+
+function createCard({title, timeframes, time = 'weekly' }){
     const card = document.createElement('li')
     card.classList.add('activity__card-item')
     card.style.backgroundColor = `var(--primary-${title.replace(/\s/g, "").toLowerCase()})`
@@ -123,8 +143,8 @@ function createCard({title, timeframes}){
             </div>
 
             <div class="activity__card-item-more-details">
-            <p class="activity__card-item-current-time">${timeframes.weekly.current}hrs</p>
-            <p class="activity__card-item-previous-time">Last Week - ${timeframes.weekly.previous}hrs</p>
+            <p class="activity__card-item-current-time">${timeframes[time].current}hrs</p>
+            <p class="activity__card-item-previous-time">Last Week - ${timeframes[time].previous}hrs</p>
             </div>
         </div>`
 
