7ab6eb35aacce23656b7efa3b5de8fe051fb104b
andresisfelipe20 <andresisfelipe20@gmail.com>
2024-10-26 13:26:02 +0000

    Add changeLevelLabl and changeLevelColor

--- password-generator-app/index.js
+++ password-generator-app/index.js
@@ -7,7 +7,7 @@ const range = document.querySelector('input[type="range"]');
 const charLengthDisplay = document.getElementById('char_length_display');
 const checkLabels = document.querySelectorAll('.label_for_check');
 const strengthLevel = document.getElementById('strength_level');
-const coloredLevel = document.getElementById('colored_level');
+const coloredLevel = document.querySelectorAll('.level');
 
 const uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 const lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
@@ -45,7 +45,7 @@ const fetchData = (form) => {
     return data;
 }
 
-function generatePassword(length, charsList) {
+const generatePassword = (length, charsList) => {
     let password = '';
     
     // Generar la contraseña
@@ -74,10 +74,60 @@ const showCopiedMsg = () => {
     }, 1500);
 }
 
-
 const handleRangeColor = (e) => {
     const position = (e.target.value*100)/e.target.max;
     range.style.background = `linear-gradient(90deg, rgba(164,255,175,1) 0%, rgba(164,255,175,1) ${position}%, rgba(24,23,31,1) ${position}%)`;
+}
+
+const toggleCheck = (e) => {
+    e.target.classList.toggle('checked');
+}
+
+const complexityLevel = (data, password) => {
+    const isUppercase = uppercase.some(item => password.includes(item));
+    const isLowercase = lowercase.some(item => password.includes(item));
+    const isNumber = numbers.some(item => password.includes(item));
+    const isSymbol = symbols.some(item => password.includes(item));
+    
+    const charType = [isUppercase, isLowercase, isNumber, isSymbol].filter(Boolean).length
+    if(data.char_length < 6 || (charType === 1 && !isSymbol)){
+        return "too_weak";
+    }
+
+    if((data.char_length >= 6 && data.char_length < 8) || (charType === 2 && !isSymbol)){
+        return "weak";
+    }
+
+    if((data.char_length >= 8 && data.char_length < 12 && charType >= 3) || (data.char_length >= 12 && charType >= 3 && !isSymbol)){
+        const symbolCount = [...password].filter(char => symbols.includes(char)).length;
+        if(symbolCount <= 3){
+            return "medium";
+        }
+    }
+    return "strong";
+}
+
+const changeLevelLabel = (level) => {
+    strengthLevel.textContent = level.toUpperCase();
+}
+
+const changeLevelColor = (strength_level) => {
+    const colors = {
+        'too_weak': 'var(--red)',
+        'weak': 'var(--orange)',
+        'medium': 'var(--yellow)',
+        'strong': 'var(--neon-green)'
+    }
+    const levels = ['too_weak','weak','medium','strong'];
+    const index = levels.indexOf(strength_level);
+
+    coloredLevel.forEach((element, i)=>{
+        element.classList.remove(...levels);
+        if(i<=index){
+            element.style.backgroundColor = colors[strength_level];
+            element.style.borderColor = colors[strength_level];
+        }
+    })
 }
 
 range.addEventListener('input', (e)=>{
@@ -93,10 +143,6 @@ copyIcon.addEventListener('click', (e) => {
         copyToClipboard(text, showCopiedMsg)
     }
 })
-
-const toggleCheck = (e) => {
-    e.target.classList.toggle('checked');
-}
 
 checkLabels.forEach((label)=>{
     label.addEventListener('click', (e)=>{
@@ -116,9 +162,9 @@ form.addEventListener('submit', (e) => {
             // const regex_pattern = buildRegex(data);
             const result = generatePassword(data.char_length, chars_list);
             pw_generated.value = result;
-            // const strength_level = complexityLevel(data, result);
-            // changeLevelLabel(strength_level);
-            // changeLevelColor(strength_level);
+            const strength_level = complexityLevel(data, result);
+            changeLevelLabel(strength_level);
+            changeLevelColor(strength_level);
         }else{
             alert('You must select at least one type of character to be included in the generated password.');
         }
