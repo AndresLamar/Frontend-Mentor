973775b136c0699d114e33e1c9651a48f9ca858a
andresisfelipe20 <andresisfelipe20@gmail.com>
2024-11-17 15:53:47 +0000

    Update game component styles

--- Tic Tac Toe game/src/components/Game/Game.tsx
+++ Tic Tac Toe game/src/components/Game/Game.tsx
@@ -1,93 +1,132 @@
-import React, { useState } from 'react';
-import './Game.css'
-
-interface SquareProps{
-    children: React.ReactNode;
-    updateBoard: (index: number) => void;
-    index: number;
-}
-
-const Square = ({children, updateBoard, index} : SquareProps) => {
-    const handleClick = () => {
-        updateBoard(index)
-    } 
-    
-    return(
-        <div onClick={handleClick} className="square" key={index}>
-            {children}
-        </div>
-    )
-}
-
-const WINNER_COMBOS = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-]
+import "./Game.css";
+import { IconLogo, IconO, IconRestart, IconX } from "../Icons/Icons";
+import { Square } from "./Square";
+import { useGame } from "../../hooks/useGame";
+import { PlayerSymbol } from "../../utils/types/types";
 
 export const Game = () => {
-    const [board, setBoard] = useState(Array(9).fill(null));
-    const [turn, setTurn ] = useState('x')
-    const [winner, setWinner] = useState<string | null>(null)
+    const {board, currentTurn, updateBoard, resetGame } = useGame();
 
-    const checkWinner = (boardToCheck : string[]) => {
-        for(const combo of WINNER_COMBOS){
-            const [a, b, c] = combo
-            if(
-                boardToCheck[a] &&
-                boardToCheck[a] === boardToCheck[b] &&
-                boardToCheck[a] === boardToCheck[c]
-            ){
-                return boardToCheck[a]
-            }
-        }
+    // useEffect(() => {
+    //     if (winner !== null || isGameOver) {
+    //       if (winner) {
+    //         updateScore(winner);
+    //         if (winner === 'X') setFlashX(true);
+    //         if (winner === 'O') setFlashO(true);
+    
+    //         setTimeout(() => {
+    //           openDialog(`Player ${winner === playerChoice ? '1' : '2'} Wins!`, 'Next Round', 'Quit', winner);
+    //           setFlashX(false);
+    //           setFlashO(false);
+    //         }, 1000);
+    //       } else {
+    //         updateScore(null);
+    //         setFlashTies(true);
+    
+    //         setTimeout(() => {
+    //           openDialog('Round Tied', 'Next Round', 'Quit', null);
+    //           setFlashTies(false);
+    //         }, 1000);
+    //       }
+    //     }
+    //   }, [winner, isGameOver, gameMode, playerChoice, openDialog, updateScore]);
+//   const [board, setBoard] = useState(() => {
+//     const storedBoard = window.localStorage.getItem("board");
+//     return storedBoard ? JSON.parse(storedBoard) : Array(9).fill(null);
+//   });
+//   const [turn, setTurn] = useState(() => {
+//     const storedTurn = window.localStorage.getItem("turn");
+//     return storedTurn ? JSON.parse(storedTurn) : "x";
+//   });
+//   const [winner, setWinner] = useState<string | null | false>(null);
 
-        return null 
-    }
+//   const checkWinner = (boardToCheck: string[]) => {
+//     for (const combo of WINNER_COMBOS) {
+//       const [a, b, c] = combo;
+//       if (
+//         boardToCheck[a] &&
+//         boardToCheck[a] === boardToCheck[b] &&
+//         boardToCheck[a] === boardToCheck[c]
+//       ) {
+//         return boardToCheck[a];
+//       }
+//     }
 
+//     return null;
+//   };
 
-    const updateBoard = (index : number) => {
-        if(board[index] || winner) return
+//   const checkEndGame = (newBoard: string[]) => {
+//     return newBoard.every((square) => square !== null);
+//   };
 
-        const newBoard = [...board]
-        newBoard[index] = turn
-        setBoard(newBoard)
+//   const updateBoard = (index: number) => {
+//     if (board[index] || winner) return;
 
-        const newTurn = turn === 'x' ? 'o' : 'x'
-        setTurn(newTurn)
+//     const newBoard = [...board];
+//     newBoard[index] = turn;
+//     setBoard(newBoard);
 
-        const newWinner = checkWinner(newBoard)
-        if(newWinner){
-            setWinner(newWinner)
-        }
-    }
+//     const newTurn = turn === "x" ? "o" : "x";
+//     setTurn(newTurn);
 
-    return (
-        <div className="board">
-            <section className="turn">
-                <p>{turn} turn</p>
-            </section>
+//     window.localStorage.setItem("board", JSON.stringify(newBoard));
+//     window.localStorage.setItem("turn", JSON.stringify(turn));
 
-            <section className="game">
-                {
-                    board.map(( _, index) => {
-                        return (
-                            <Square 
-                                key={index} 
-                                updateBoard={updateBoard} 
-                                index={index}
-                            >
-                                {board[index]}
-                            </Square>
-                        )
-                    })
-                }
-            </section>
+//     const newWinner = checkWinner(newBoard);
+//     if (newWinner) {
+//       setWinner(newWinner);
+//     } else if (checkEndGame(newBoard)) {
+//       setWinner(false);
+//     }
+//   };
+
+//   const resetGame = () => {
+//     setBoard(Array(9).fill(null));
+//     setTurn("x");
+//     setWinner(null);
+//     window.localStorage.removeItem("board");
+//     window.localStorage.removeItem("turn");
+//   };
+
+  return (
+    <div className="board">
+      <section className="board__header">
+        <div className="game__logo">
+          <IconLogo />
         </div>
-    )
-}
+        {currentTurn == PlayerSymbol.X ? (
+          <span className="turn__text">
+            <IconX /> turn
+          </span>
+        ) : (
+          <span className="turn__text">
+            <IconO /> turn
+          </span>
+        )}
+
+        <button onClick={resetGame} className="restart__button">
+          <IconRestart />
+        </button>
+      </section>
+
+      <section className="game">
+        {board.map((_, index) => {
+          return (
+            <Square key={index} updateBoard={updateBoard} index={index}>
+              {board[index] == PlayerSymbol.X ?
+                <div className="icon-x icon">
+                    <IconX />
+                </div>
+                : board[index] == PlayerSymbol.O ?
+                <div className="icon-o icon">
+                  <IconO />
+                </div>
+                  : ''
+              }
+            </Square>
+          );
+        })}
+      </section>
+    </div>
+  );
+};
