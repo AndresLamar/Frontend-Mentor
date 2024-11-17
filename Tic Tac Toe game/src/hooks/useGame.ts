4f89e573ff1341304c0c101b027c9d489ab74ff0
andresisfelipe20 <andresisfelipe20@gmail.com>
2024-11-17 15:54:24 +0000

    Refactor game logic to a custom hook

---
+++ Tic Tac Toe game/src/hooks/useGame.ts
@@ -0,0 +1,71 @@
+import { useCallback, useState } from "react"
+import { PlayerSymbol } from "../utils/types/types"
+import useLocalStorage from "./useLocalStorage"
+import { checkWinner } from "../utils/logic/logic";
+
+export const useGame  = () => {
+    const [board, setBoard] = useLocalStorage<Array<'X' | 'O' | null>>('board', Array(9).fill(null));
+    const [initialTurn, setInitialTurn] = useLocalStorage<PlayerSymbol>('initialTurn', PlayerSymbol.X);
+    const [currentTurn, setCurrentTurn] = useLocalStorage<PlayerSymbol>('currentTurn', initialTurn);
+    const [winner, setWinner] = useState<PlayerSymbol | null | false>(null);
+    const [isGameOver, setIsGameOver] = useState(false);
+
+    // const checkWinner = (boardToCheck: Array<'X' | 'O' | null>) => {
+    //     for (const combo of WINNER_COMBOS) {
+    //       const [a, b, c] = combo;
+    //       if (
+    //         boardToCheck[a] &&
+    //         boardToCheck[a] === boardToCheck[b] &&
+    //         boardToCheck[a] === boardToCheck[c]
+    //       ) {
+    //         return boardToCheck[a];
+    //       }
+    //     }
+    
+    //     return null;
+    // };
+    
+    const checkEndGame = (newBoard: Array<'X' | 'O' | null>) => {
+      return newBoard.every((square) => square !== null);
+    };
+
+    const updateBoard = useCallback((index: number) => {
+        if (board[index] || isGameOver) return;
+
+        const newBoard = [...board];
+        newBoard[index] = currentTurn;
+        setBoard(newBoard);
+
+        const newTurn = currentTurn === PlayerSymbol.X ? PlayerSymbol.O : PlayerSymbol.X;
+        setCurrentTurn(newTurn);
+
+        const { winner } = checkWinner(newBoard);
+        if (winner) {
+            setWinner(winner as PlayerSymbol);
+            setInitialTurn((prevTurn) => (prevTurn === PlayerSymbol.X ? PlayerSymbol.O : PlayerSymbol.X))
+            setIsGameOver(true);
+        } else if (checkEndGame(newBoard)) {
+            setWinner(false);
+            setIsGameOver(true);
+        }
+    }, [board, currentTurn, isGameOver, setCurrentTurn, setInitialTurn]);
+
+    const resetGame = useCallback(() => {
+        setBoard(Array(9).fill(null));
+        setWinner(null);
+        setInitialTurn(PlayerSymbol.X);
+        setCurrentTurn(PlayerSymbol.X);
+        setIsGameOver(false);
+    }, []);
+
+    return {
+        currentTurn,
+        setCurrentTurn,
+        board,
+        winner,
+        updateBoard,
+        resetGame,
+        initialTurn,
+        isGameOver
+      };
+}
