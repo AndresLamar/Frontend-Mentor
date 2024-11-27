87e29fef7416840e1f46c4685c2ab6e9a0e30b26
andresisfelipe20 <andresisfelipe20@gmail.com>
2024-11-27 18:39:37 +0000

    Add mobile navigation

---
+++ room-homepage/scipt.js
@@ -0,0 +1,47 @@
+const btnOpen = document.getElementById("btnOpen");
+const btnClose = document.getElementById("btnClose");
+const media = window.matchMedia('(min-width: 48rem)');
+const navMenu = document.querySelector('.js-menu');
+const main = document.querySelector('main')
+const body = document.querySelector('body')
+
+function setupTopNav(e){
+    if(e.matches){
+        navMenu.removeAttribute('inert')
+    } else{
+        navMenu.setAttribute('inert', '')
+        navMenu.style.transition = 'none';
+    }
+}
+
+function openMobileMenu(){
+    btnOpen.setAttribute('aria-expanded', 'true')
+    navMenu.removeAttribute('inert')
+    navMenu.removeAttribute('style')
+    main.setAttribute('inert', '')
+    body.classList.add('menu-open')
+    bodyScrollLockUpgrade.disableBodyScroll(body);
+    btnClose.focus()
+}
+
+function closeMobileMenu(){
+    btnOpen.setAttribute('aria-expanded', 'false')
+    navMenu.setAttribute('inert', '')
+    main.removeAttribute('inert')
+    body.classList.remove('menu-open')
+    bodyScrollLockUpgrade.enableBodyScroll(body);
+    btnOpen.focus()
+
+    setTimeout(() => {
+        navMenu.style.transition = 'none';
+    }, 500)
+}
+
+setupTopNav(media)
+
+btnOpen.addEventListener('click', openMobileMenu)
+btnClose.addEventListener('click', closeMobileMenu)
+
+media.addEventListener('change', function(e){
+    setupTopNav(e)
+})
