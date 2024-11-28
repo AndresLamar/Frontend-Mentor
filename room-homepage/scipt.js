e01c1ae92f07c7a048b217226f1824eb0c3610e1
andresisfelipe20 <andresisfelipe20@gmail.com>
2024-11-28 21:47:26 +0000

    Add animations to slide

--- room-homepage/scipt.js
+++ room-homepage/scipt.js
@@ -2,8 +2,70 @@ const btnOpen = document.getElementById("btnOpen");
 const btnClose = document.getElementById("btnClose");
 const media = window.matchMedia('(min-width: 48rem)');
 const navMenu = document.querySelector('.js-menu');
-const main = document.querySelector('main')
-const body = document.querySelector('body')
+const body = document.querySelector('body');
+const main = document.querySelector('main');
+
+const card = document.querySelector('.js-card');
+const pictures = document.querySelectorAll('.js-picture');
+const cardTexts = document.querySelectorAll('.js-card-text');
+const previousBtn = document.querySelector('.js-previous-button');
+const nextBtn = document.querySelector('.js-next-button');
+
+function getNewIndex(currentIndex, direction, totalItems) {
+    if (direction === 'next') {
+        return (currentIndex + 1) % totalItems; // Avanza al siguiente índice cíclicamente
+    } else if (direction === 'previous') {
+        return (currentIndex - 1 + totalItems) % totalItems; // Retrocede al índice anterior cíclicamente
+    }
+    return currentIndex; // En caso de que la dirección sea inválida
+}
+
+
+function updateThumbnail(direction){
+    let currentIndex = -1;
+
+    // Encuentra el índice de la imagen actual
+    pictures.forEach((picture, index) => {
+        if (picture.classList.contains('current')) {
+            currentIndex = index;
+            picture.classList.remove('current'); // Elimina la clase `current`
+        }
+    });
+
+    // Calcula el nuevo índice usando la función centralizada
+    const newIndex = getNewIndex(currentIndex, direction, pictures.length);
+
+    // Agrega la clase `current` al elemento correspondiente
+    pictures[newIndex].classList.add('current');
+}
+
+function updateText(direction){
+    let currentIndex = -1;
+
+    // Encuentra el índice de la imagen actual
+    cardTexts.forEach((picture, index) => {
+        if (picture.classList.contains('current')) {
+            currentIndex = index;
+            picture.classList.remove('current'); // Elimina la clase `current`
+        }
+    });
+
+    // Calcula el nuevo índice usando la función centralizada
+    const newIndex = getNewIndex(currentIndex, direction, cardTexts.length);
+
+    // Agrega la clase `current` al elemento correspondiente
+    cardTexts[newIndex].classList.add('current');
+}
+
+nextBtn.addEventListener('click', () => {
+    updateThumbnail('next')
+    updateText('next')
+});
+
+previousBtn.addEventListener('click', () => {
+    updateThumbnail('previous')
+    updateText('previous')
+});
 
 function setupTopNav(e){
     if(e.matches){
